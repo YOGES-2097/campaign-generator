8050c70ef4dae7c6f6e0c48503c8153a429d5080
YOGES-2097 <yogeswaranm2907@gmail.com>
2026-03-08 22:14:27 +0530

    testing completion

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google import genai
 
-API_KEY = ""  # Add your API key here
+API_KEY = "AIzaSyBivc1qfXEj-448puvcis2tIM5Dc0CFK2w"  # Add your API key here
 
 st.set_page_config(page_title="AI Campaign Generator", layout="wide")
 
